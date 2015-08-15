2107d15f523529d671694d534fa4e23b22ede754
David Conner <dconner.pro@gmail.com>
2015-08-15 15:09:15 -0400

    experimented with pulling data out of open trip planner

--- otp-transitive/index.js
+++ otp-transitive/index.js
@@ -16,9 +16,36 @@ var od = {
 
 // Create new instance
 var profiler = new Profiler({
-  host: 'http://localhost:8080/otp/routers/default'
+  host: 'http://localhost:8080/otp/routers/default',
+  limit: 3
 });
 
-profiler.routes(function(err, routes) {
-  console.log(routes); // Outputs all usable transit routes
+var profile;
+var opts = {
+  from: {
+    lat: 37.40,
+    lon: -80.20
+  },
+  to: {
+    lat: 37.20,
+    lon: -79.75
+  }
+};
+
+profiler.journey(opts, function(err, data) {
+  console.log(data);
 });
+
+//profiler.profile(opts, function(err, p) {
+//  profile = p;
+//  console.log(p); // Outputs all usable transit routes
+//});
+
+//profiler.routes(function(err, routes) {
+//  console.log(routes); // Outputs all usable transit routes
+//});
+
+//
+//profiler.patterns(profile, function(err, p) {
+//  console.log(p); // Outputs all usable transit routes
+//});
