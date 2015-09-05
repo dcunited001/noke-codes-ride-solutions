5082807222d55e48dfcabb8ca1aedad4375d3878
David Conner <dconner.pro@gmail.com>
2015-09-05 15:38:25 -0400

    * setup a basic angular app with ui-map
    * added MapCtrl and onGoogleReady callback to app.js

--- gtfs-to-kml/app/js/app.js
+++ gtfs-to-kml/app/js/app.js
@@ -1,1 +1,23 @@
+angular.module('app', ['ui.router', 'ui.event', 'ui.map'])
 
+    .config(function ($stateProvider, $locationProvider, $UrlRouterProvider) {
+
+    })
+
+    .controller('MapCtrl', function ($scope) {
+      $scope.mapOptions = {
+        center: new google.maps.LatLng(35.784, -78.670),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+    })
+
+    .run(function ($rootScope) {
+
+    })
+
+;
+
+function onGoogleReady() {
+  angular.bootstrap(document.getElementById("map"), ['app.ui-map']);
+}
